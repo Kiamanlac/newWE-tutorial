f9c45495ac1214aa7226459dbba2bb3044749283
Kiamanlac <eruzbrr@protonmail.com>
2024-03-20 09:43:46 +0100

    finished lesson 3

--- app.js
+++ app.js
@@ -3,41 +3,109 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-
+ 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const shoppingListsRouter = require('./routes/shoppingLists')
-
+ 
 const app = express()
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+ 
+// WE Ergaenzungen
+app.set('Hello', 'world')
+console.log(app.get('Hello')) // 'world'
+ 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-
+ 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/shopping-lists', shoppingListsRouter)
-
+ 
+//WE Ergaenzung
+//app.use('/top-secret', req => console.log(req.originalUrl))
+ 
+//WE Ergaenzung
+app.use('/top-secret', (req, res, next) => {
+    console.log(`Someone entered the secretroute: ${req.originalUrl}`)
+    next()
+    console.log('This is after next()')
+})
+ 
+app.use('/top-secret', (req, res, next) => {
+    res.status(403).send('Access denied! 🚫');
+    return
+})
+ 
+app.use('/next-test', (req, res, next) => {
+    console.log('before next() 😺')
+    next()
+    console.log('after next() 🐡')
+})
+ 
+app.use('/next-test', (req, res, next) => {
+    console.log('2nd middleware 🐶 wuff wuff !!!')
+})
+ 
+app.use('/get-route/:age', (req, res, next) => {
+    if (req.params.age < 18) {
+        res.status(403).send('Sorry, you have to be at least 18 years old to continue. 😥')
+        return
+    }
+    next()
+})
+ 
+app.get('/get-route/:age', (req, res, next) => {
+    res.send('Adult zone! 🍺')
+})
+ 
+app.post('/post-route', (req, res, next) => {
+    res.send('data send successfully 🐱‍💻🏴‍☠️🔥🍮🐞')
+})
+ 
+app.post('/login-route1', (req, res, next) => {
+    if (req.body.username === 'admin' && req.body.password === 'admin') {
+        res.send('Login successful! 🎉')
+        return
+    }
+    res.status(403).send('Login failed! 😥')
+})
+ 
+app.use('/login-route', (req, res, next) => {
+    if (req.body.username && req.body.password) {
+        console.log('username: ' + req.body.username)
+        console.log('password: ' + req.body.password)
+        next()
+        return
+    }
+    res.status(422).send('cannot process data 🤔')
+    return
+})
+ 
+app.post('/login-route', (req, res) => {
+    res.send('login successful 🐱‍🏍')
+})
+ 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404))
 })
-
+ 
 // error handler
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message
     res.locals.error = req.app.get('env') === 'development' ? err : {}
-
+ 
     // render the error page
     res.status(err.status || 500)
     res.render('error')
 })
-
+ 
 module.exports = app
--- routes/_shoppingLists1.js
+++ routes/_shoppingLists1.js
@@ -5,36 +5,35 @@ const {
     updateShoppingListFor,
     removeShoppingListEntryFor,
 } = require('fs-json')
-
-const router = express.Router()
+// Übung 12 Kap 3
 
 router.get('/:id', (req, res, next) => {
-    const { id } = req.params
+    const { id } = req.params;
     res.send(`Shopping list for User ${id}: ${readShoppingListOf(id)}`)
 })
 
 router.post('/', (req, res, next) => {
     const { id, list } = req.body
     saveShoppingListFor(id, list)
-    res.send(`Saved new shopping list for User ${id} successfully 💾!`)
+    res.send(`Saved new shopping list for User ${id} successfully!🤷‍♂️🤷‍♂️🤷‍♂️`)
 })
 
 router.put('/', (req, res, next) => {
     const { id, list } = req.body
-    updateShoppingListFor(id, list)
-    res.send(`Updated shopping list for User ${id} successfully!`)
+    updateShoppingListFor(id, list);
+    res.send(`Updated shopping list for User ${id} successfully!🤦‍♂️🤦‍♂️🤦‍♂️`)
 })
 
 router.delete('/:id', (req, res, next) => {
     const { id } = req.params
-    removeShoppingListEntryFor(id)
-    res.send(`Deleted shopping list for User ${id} successfully🪓!`)
+    removeShoppingListEntryFor(id);
+    res.send(`Deleted shopping list for User ${id} successfully!🐱‍👤🐱‍👤🐱‍👤`)
 })
 
 router.delete('/:id/:entry', (req, res, next) => {
     const { id, entry } = req.params
-    removeShoppingListEntryFor(id, entry)
-    res.send(`Removed ${entry} from User ${id}'s shopping list successfully!🪓`)
+    removeShoppingListEntryFor(id, entry);
+    res.send(`Removed ${entry} from User ${id}'s shopping list successfully!`)
 })
 
 module.exports = router
