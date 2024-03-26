ec1a717580038da0397f129fe97e01e9d94caa93
Kiamanlac <eruzbrr@protonmail.com>
2024-03-26 15:50:53 +0100

    nachUebung29

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>'] })
+//router.get('/', (req, res, next) => {
+//    res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>'] })
+//})
+
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express',
+        content: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    })
 })
 
 module.exports = router
